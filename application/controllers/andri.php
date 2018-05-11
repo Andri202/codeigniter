e3e8856322bd743d3f1448c173c08607597603b8
Andri Gunawan <andrign20@gmail.com>
2018-05-11 20:32:24 +0700

    htaccess

--- application/controllers/andri.php
+++ application/controllers/andri.php
@@ -3,12 +3,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class andri extends CI_Controller {
 	
-	public function index()
+	public function Index()
 	{
 		echo "Ini adalah controller Andri";
 	}
 
-	public function profil()
+	public function Profil()
 	{
 		echo "Profil saya adalah ini";
 	}
