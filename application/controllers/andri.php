c086b99976fae0f7e20a769b36894e280eda4064
Andri Gunawan <andrign20@gmail.com>
2018-05-13 14:00:02 +0700

    edit route

--- application/controllers/andri.php
+++ application/controllers/andri.php
@@ -3,12 +3,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class andri extends CI_Controller {
 	
-	public function Index()
+	public function index()
 	{
 		echo "Ini adalah controller Andri";
 	}
 
-	public function Profil()
+	public function controllerProfil()
 	{
 		echo "Profil saya adalah ini";
 	}
